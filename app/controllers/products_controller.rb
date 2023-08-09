90e3ec61d59e79d4101ed0861c7bebf68c123ede
Julio Miguel Gagliardi <juliog_14@hotmail.com>
2023-08-08 23:04:37 -0300

    Fix rubocop linters

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,19 +6,19 @@ class ProductsController < ApplicationController
   def search
     search_term = params[:search].strip
     decoded_search_term = CGI.unescape(search_term) # Decode the search term
-    SearchQuery.create(term: decoded_search_term) if decoded_search_term.include?("?")
-    decoded_search_term_whitout_symbols = decoded_search_term.gsub(/[[:punct:]]/, "")
+    SearchQuery.create(term: decoded_search_term) if decoded_search_term.include?('?')
+    decoded_search_term_whitout_symbols = decoded_search_term.gsub(/[[:punct:]]/, '')
 
-    if decoded_search_term.present?
-      @products = Product.where("name LIKE ?", "%#{decoded_search_term_whitout_symbols}%")
-    else
-      @products = Product.all
-    end
+    @products = if decoded_search_term.present?
+                  Product.where('name LIKE ?', "%#{decoded_search_term_whitout_symbols}%")
+                else
+                  Product.all
+                end
 
     if request.xhr?
-      render partial: "products_list", locals: { products: @products }
+      render partial: 'products_list', locals: { products: @products }
     else
-      render "index"
+      render 'index'
     end
   end
 end
