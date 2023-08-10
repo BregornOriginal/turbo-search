7a6a3f36a7b0784be1bdb6aea1823e638da20b20
Julio Miguel Gagliardi <juliog_14@hotmail.com>
2023-08-10 02:03:20 -0300

    Add cancancan and relations to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+User.create(name: "First user", surname: "seed", email: "new@new.com", password: "123456", created_at: Date.new, updated_at: Date.new)
+
+Product.create(name: "First product good", description: "this prodcut is good", price: 1, user_id: 1)
+Product.create(name: "First product bad", description: "this prodcut is bad", price: 1, user_id: 1)
+Product.create(name: "Second product good", description: "this prodcut is good", price: 1, user_id: 1)
+Product.create(name: "Second product bad", description: "this prodcut is bad", price: 1, user_id: 1)
+Product.create(name: "Third product good", description: "this prodcut is good", price: 1, user_id: 1)
+Product.create(name: "Third product bad", description: "this prodcut is bad", price: 1, user_id: 1)
